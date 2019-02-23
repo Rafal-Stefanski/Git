08ef66f23c615ff18facd5fc42730dbc5212041a
rafal-rs <rafal-stefanski@wp.pl>
2019-02-23 12:21:39 +0100

    init

---
+++ src/java_basic.java
@@ -0,0 +1,19 @@
+/**
+ * To mój pierwszy program
+ * nie wiem dlaczego, ale działa :)
+ */
+
+public class java_basic {
+    public static void main(String[] args) {
+        // tutaj będzie witaj
+        System.out.println("Witaj,");
+        // tutaj będzie imię i nazwisko
+        System.out.println("Rafał Stefański");
+
+        /*
+        komentarz wielowierszowy
+        dla testowania.
+         */
+
+    }
+}
