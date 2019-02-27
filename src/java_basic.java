372eec94e58b040e62a2eeb4fc00f9b2b679e68f
RS <rafal-stefanski@wp.pl>
2019-02-27 15:24:18 +0100

    testowy commit

--- src/java_basic.java
+++ src/java_basic.java
@@ -1,8 +1,21 @@
-/*
+/**
+ * Komentarz wielowierszowy do tworzenia z niego dokumentacji:
+ * Przykładowy tekst:
+ * To mój pierwszy program,
+ * działa,
+ * choć nie wiem jeszcze dlaczego :)
+ */
+
 public class java_basic {
     public static void main(String[] args) {
-
+        // tutaj będie witaj.
         System.out.println("Witaj,");
-
+        //tutaj będzie imię i nazwisko.
         System.out.println("Rafał Stefański");
-*/
+    }
+
+    /*
+    komentarz wielowierszowy
+    dla testu.
+     */
+}
--- src/testowa_do_przeniesienia.java
+++
@@ -1,5 +0,0 @@
-public class testowa_do_przeniesienia {
-    public static void main(String[] args) {
-        System.out.println("testowo");
-    }
-}
