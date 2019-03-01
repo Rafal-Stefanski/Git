6ddce71a9afe36144a47071f3e32b7ae39dc3f2f
rafal-rs <rafal-stefanski@wp.pl>
2019-03-01 20:47:12 +0100

    Zadania #statements

--- src/Zadania_statements/Task2.java
+++ src/Zadania_statements/Task2.java
@@ -1,22 +1,28 @@
 package Zadania_statements;
 
-/**
+/*
  * 2. Napisz program, który wypisze na ekran konsoli, słowo oznaczające ocenę dla podanej cyfry.
  * Np. dla 1 - "niedostateczny", 2 - "mierny" itp. Obsłuż przypadek gdy cyfra jest poza skalą ocen.
  */
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int a = 4;
+        System.out.print("Podaj ocenę: ");
+        int a = scanner.nextInt();
+        System.out.println();
+        System.out.println("ocena " + a );
 
-        if (a == 1) System.out.println("niedostateczny");
-        if (a == 2) System.out.println("mierny");
-        if (a == 3) System.out.println("dostateczny");
-        if (a == 4) System.out.println("dobry");
-        if (a == 5) System.out.println("bardzo dobry");
-        if (a == 6) System.out.println("celujący");
-        if (a < 1 || a > 6) System.out.println("ocena poza skalą");
+        if (a == 1) System.out.println("niedostateczna");
+        if (a == 2) System.out.println("mierna");
+        if (a == 3) System.out.println("dostateczna");
+        if (a == 4) System.out.println("dobra");
+        if (a == 5) System.out.println("bardzo dobra");
+        if (a == 6) System.out.println("celująca");
+        if (a < 1 || a > 6) System.out.println("jest poza skalą");
 
     }
 }
--- src/Zadania_statements/Task3.java
+++ src/Zadania_statements/Task3.java
@@ -1,24 +1,32 @@
 package Zadania_statements;
 
-/**
+/*
  * 3. Napisz program, który wypisze na ekran konsoli, cyfrę arabską dla podanej liczby rzymskiej (od 1 do 9).
  * Czyli np. dla 'I' wypisze 1, dla 'V' 5 itp. Obsłuż przypadek gdy podana liczba rzymska jest nieprawidłowa.
  */
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-        int b=6;
-        if (b==1) System.out.println("I");
-        if (b==2) System.out.println("II");
-        if (b==3) System.out.println("III");
-        if (b==4) System.out.println("IV");
-        if (b==5) System.out.println("V");
-        if (b==6) System.out.println("VI");
-        if (b==7) System.out.println("VII");
-        if (b==8) System.out.println("VIII");
-        if (b==9) System.out.println("IX");
-        if (b==10) System.out.println("X");
-        if (b<1||b>10) System.out.println("Out of scale, only 1-10 numbers");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę całkowitą arabską w zakresie 1-10: ");
+        int b = scanner.nextInt();
+        System.out.println();
+        System.out.println("Dla liczy " + b + ", liczba rzymska to: ");
+
+        if (b == 1) System.out.println("I");
+        if (b == 2) System.out.println("II");
+        if (b == 3) System.out.println("III");
+        if (b == 4) System.out.println("IV");
+        if (b == 5) System.out.println("V");
+        if (b == 6) System.out.println("VI");
+        if (b == 7) System.out.println("VII");
+        if (b == 8) System.out.println("VIII");
+        if (b == 9) System.out.println("IX");
+        if (b == 10) System.out.println("X");
+        if (b < 1 || b > 10) System.out.println("Out of scale, only 1-10 numbers");
 
     }
 }
