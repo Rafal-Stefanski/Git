890d32ccf3c83bea027800c1f071ec6c27719496
rafal-rs <rafal-stefanski@wp.pl>
2019-03-01 21:03:12 +0100

    Zadania #datatypes

---
+++ src/Testy_i_próby/Zadania_datatypes_TS/Task2TS.java
@@ -0,0 +1,15 @@
+package Testy_i_próby.Zadania_datatypes_TS;
+
+
+
+// Wersja do Tomasz Sidor
+
+
+public class Task2TS {
+    public static void main(String[] args) {
+        int a = 2;
+
+        System.out.println("a do trzeciej = " + a * a * a);
+
+    }
+}
