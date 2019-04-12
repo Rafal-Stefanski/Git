a681f04c1a2da9d54e4d004fd0272d22c103ab43
RS <rafal-stefanski@wp.pl>
2019-04-12 18:28:28 +0200

    Zadania # hackerank

--- src/main/java/pl/sda/testy_i_próby/Solution.java
+++ src/main/java/pl/sda/testy_i_próby/Solution.java
@@ -11,15 +11,22 @@ public class Solution {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if (N % 2 != 0 && N >= 1 && N <= 100) {
-            System.out.println("Weird");
-            if (N % 2 == 0 && N >= 2 && N < 5) {
+        if (N >= 1 && N <= 100) {
+            if (N % 2 == 0 && N < 5) {
+                System.out.println("Not Weird");
             }
-            System.out.println("Not Weird");
+            if (N % 2 == 0 && N > 20) {
+                System.out.println("Not Weird");
+            }
+            if (N % 2 == 0 && N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            }
+            if (N % 2 != 0) {
+                System.out.println("Weird");
+            }
         } else {
             System.out.println("Weird");
         }
-
 
         scanner.close();
     }
