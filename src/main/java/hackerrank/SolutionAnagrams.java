36b4576e6413e09409eba08ea0aaa623ac4ba4b8
RS <rafal-stefanski@wp.pl>
2019-04-23 17:40:56 +0200

    Zadania # hackerrank

---
+++ src/main/java/hackerrank/SolutionAnagrams.java
@@ -0,0 +1,24 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class SolutionAnagrams {
+//    static boolean isAnagram(String a, String b) {
+//        // Complete the function
+//        a = a.toLowerCase();
+//        b = b.toLowerCase();
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        Scanner scan = new Scanner(System.in);
+//        String a = scan.next();
+//        String b = scan.next();
+//        scan.close();
+//        boolean ret = isAnagram(a, b);
+//        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+//    }
+
+
+}
