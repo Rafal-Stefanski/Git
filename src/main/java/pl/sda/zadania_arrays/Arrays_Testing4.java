21b2752386120bdbb11689e289254e24e5dd70ce
RS <rafal-stefanski@wp.pl>
2019-06-04 19:24:40 +0200

    Zadania # Arrays

---
+++ src/main/java/pl/sda/zadania_arrays/Arrays_Testing4.java
@@ -0,0 +1,15 @@
+package pl.sda.zadania_arrays;
+
+public class Arrays_Testing4 {
+    public static void main(String[] args) {
+        int[] ints;
+        ints = new int[5];
+        ints[0] = 12;
+        ints[1] = 23;
+        ints[2] = 34;
+        ints[3] = 45;
+        ints[5] = 56;
+
+        System.out.println(ints[1]);
+    }
+}
