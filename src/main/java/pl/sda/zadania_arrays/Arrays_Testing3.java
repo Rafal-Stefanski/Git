2d7563e48333f7ba91869041573f33f472a18db1
rafal-rs <rafal-stefanski@wp.pl>
2019-06-06 23:13:05 +0200

    update #array

--- src/main/java/pl/sda/zadania_arrays/Arrays_Testing3.java
+++ src/main/java/pl/sda/zadania_arrays/Arrays_Testing3.java
@@ -1,5 +1,27 @@
 package pl.sda.zadania_arrays;
 
 public class Arrays_Testing3 {
-    ArraysMethods tabliceArrays = new ArraysMethods();
+    public static void main(String[] args) {
+        String[] nazwy;
+        nazwy = new String[6];
+        nazwy[0] = "Lenovo";
+        nazwy[1] = "Samsung";
+        nazwy[2] = "Apple";
+        nazwy[3] = "Huawei";
+        nazwy[4] = "Xiaomi";
+        nazwy[5] = "Roborock";
+        System.out.print("Wywołanie 1 wartosci: ");
+        System.out.println(nazwy[3]);
+        System.out.print("Wywołanie całej tabeli: ");
+        for (int i = 0; i < nazwy.length; i++) {
+            System.out.print(nazwy[i] + " ");
+        }
+
+        String[]nazwyRazem = new String[]{"nazwa Lenovo","Samsung", "Apple", "Huawei", "Xiaomi", "Roborock"};
+        System.out.println("n/Deklaracja tablicy razem z innicjalizacją");
+        for (String i : nazwyRazem) {
+            System.out.print(i + " ");
+        }
+
+    }
 }
