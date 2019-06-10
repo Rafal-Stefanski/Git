05bb3ab66c9c726256452610fc2f43193030fb27
rafal-rs <rafal-stefanski@wp.pl>
2019-06-11 00:29:57 +0200

    update #array

--- src/main/java/pl/sda/zadania_arrays/Arrays_Testing4.java
+++ src/main/java/pl/sda/zadania_arrays/Arrays_Testing4.java
@@ -15,5 +15,14 @@ public class Arrays_Testing4 {
         for (int i = 0; i < tabelka.length; i++) {
             System.out.println("pos: " + i + ", value: " + tabelka[i]);
         }
+
+        int[] table2 = new int[]{12, 23, 34, 45, 56};
+        System.out.println("Table values in one line: ");
+        for (int i = 0; i < table2.length; i++) {
+            System.out.print(table2[i] + " ");
+        }
+        System.out.println("Table length: " + table2.length);
+        System.out.println("item on index 2: " + table2[1]);
+
     }
 }
