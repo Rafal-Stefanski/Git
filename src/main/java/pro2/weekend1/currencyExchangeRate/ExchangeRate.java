ceb02e6b1b48624706249f9c767b9fea4cf3d263
rafal-rs <rafal-stefanski@wp.pl>
2019-07-03 23:18:24 +0200

    update #programowanie 2

--- src/main/java/pro2/weekend1/currencyExchangeRate/ExchangeRate.java
+++ src/main/java/pro2/weekend1/currencyExchangeRate/ExchangeRate.java
@@ -4,29 +4,54 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
 
 public class ExchangeRate {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String currencyValue = downLoadexchangeRate();
+        System.out.println("The currency rate for today is: " + currencyValue);
+
 
     }
 
-    private static String downloadExchangeRates() throws IOException {
-        String link = "http://api.nbp.pl/api/exchangerates/tables/";
+    private static String downLoadexchangeRate() throws IOException {
+//        String link = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json"; // wszystkie
+        String link = "http://api.nbp.pl/api/exchangerates/rates/a/chf//?format=json/"; //frank
+
 
         URL url = new URL(link);
-        URLConnection connect = url.openConnection();
-        connect.addRequestProperty("User-Agent", "Chrome");
-        InputStream inputStream = connect.getInputStream();
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent", "Firefox");
+        InputStream inputStream = connection.getInputStream();
         Scanner scanner = new Scanner(inputStream);
         String line = scanner.nextLine();
 
         Gson gson = new Gson();
+        NBP courency = gson.fromJson(line, NBP.class);
 
-//        NBP nbp = gson.fromJson(value, NBP.class);
-
-        return null;
+        return courency.value;
     }
+
+//    private static String downloadExchangeRates() throws IOException {
+//        String link = "http://api.nbp.pl/api/exchangerates/rates/a/chf//?format=json/"; //frank
+//        String link = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json"; // wszystkie
+//
+//        URL url = new URL(link);
+//        URLConnection connect = url.openConnection();
+//        connect.addRequestProperty("User-Agent", "Chrome");
+//        InputStream inputStream = connect.getInputStream();
+//        Scanner scanner = new Scanner(inputStream);
+//        String line = scanner.nextLine();
+//
+//        Gson gson = new Gson();
+//
+////        NBP nbp = gson.fromJson(value, NBP.class);
+//
+//        return null;
+//    }
+
+
 }
--- src/main/java/pro2/weekend1/currencyExchangeRate/NBP.java
+++ src/main/java/pro2/weekend1/currencyExchangeRate/NBP.java
@@ -1,6 +1,10 @@
 package pro2.weekend1.currencyExchangeRate;
 
 public class NBP {
+    public String table;
+    public String currency;
+    public String no;
+    public String effectiveDate;
     public String value;
 
     public NBP() {
