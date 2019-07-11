8b562e483556ecd7700961364434767991dd5ce6
RS <rafal-stefanski@wp.pl>
2019-07-11 19:01:50 +0200

    Zadania # cwiczenia z obiektow

---
+++ src/main/java/testy_i_próby/obiektowosc/Person.java
@@ -0,0 +1,21 @@
+package testy_i_próby.obiektowosc;
+
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+
+    private static int ileRazy;
+
+    void przedstawSie(boolean formal) {
+        ileRazy++;
+        if (formal) {
+            System.out.println("Dzień dobry, mam na imię " + name + ". Mój wiek to " + age + " lat.");
+        } else {
+            System.out.println("Cześć, jestem " + name + ". Mam " + age + " lat.");
+        }
+    }
+    static int ileRazySiePrzedstawili(){
+        return ileRazy;
+    }
+}
