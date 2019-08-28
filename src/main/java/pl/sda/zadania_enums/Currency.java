cf5f55f8cf6aa999f118115339bc728322bb3331
rafal-rs <rafal-stefanski@wp.pl>
2019-08-28 23:01:54 +0200

    aktualizacja zadania enum

--- src/main/java/pl/sda/zadania_enums/Currency.java
+++ src/main/java/pl/sda/zadania_enums/Currency.java
@@ -3,8 +3,11 @@ package pl.sda.zadania_enums;
 /**
  * 1. Utwórz enum Currency, który ma reprezentować walutę. Ograniczmy się do 5 walut: polski złoty,
  * dolar, euro, jen, funt brytyjski.
- *
+ * <p>
  * 3. Do enum Currency dodaj pole oznaczające symbol waluty: "PLN", "USD" itp.
+ * <p>
+ * 5. Do enum Currency dodaj pole oznaczające kurs waluty (w stosunku do polskiego złotego)
+ * oraz metodę która wyliczy wartość podanej kwoty w obcej walucie (np.: 100 PLN ~ 23 EUR)
  */
 
 public enum Currency {
@@ -20,5 +23,23 @@ public enum Currency {
     Currency(String symbol, double exchangeRate) {
         this.symbol = symbol;
         this.exchangeRate = exchangeRate;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    //Exchange rate PLN -> EUR/USD itd
+    public double ExchangeFromPLN(double value) {
+        return (1/getExchangeRate())*value;
+    }
+
+    //Exchange rate EUR/USD itd -> PLN
+    public double ExchangeToPLN(double value) {
+        return getExchangeRate()*value;
     }
 }
