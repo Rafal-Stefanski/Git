14475079792bd8cad47606f97afa9bfbf25989dc
RS <rafal-stefanski@wp.pl>
2019-09-12 15:49:37 +0200

    Udemy - kurs programming masterclas

--- src/main/java/udemy/java_programming_masterclass/section3/CharAndBollean.java
+++ src/main/java/udemy/java_programming_masterclass/section3/CharAndBollean.java
@@ -2,6 +2,24 @@ package udemy.java_programming_masterclass.section3;
 
 public class CharAndBollean {
     public static void main(String[] args) {
-        char myChar = 'D';
+
+        // Char
+        char myChar = 'D'; // char i letter D
+        char myUnicodeChar = '\u0044'; // char D as above in Unicode
+        System.out.println("D as letter D: " + myChar);
+        System.out.println("D as Unicode: " + myUnicodeChar);
+
+        // copyright symbol in Unicode
+        char myCopyrightChar = '\u00A9';
+        System.out.println("Copyright char from Unicode: " + myCopyrightChar);
+
+        // boolean
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+
     }
 }
