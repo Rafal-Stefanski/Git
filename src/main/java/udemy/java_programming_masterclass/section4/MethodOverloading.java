c146b141c1c0a3897654035d902a2bf3330ae652
rafal-rs <rafal-stefanski@wp.pl>
2019-09-25 22:52:49 +0200

    #zadania udemy

--- src/main/java/udemy/java_programming_masterclass/section4/MethodOverloading.java
+++ src/main/java/udemy/java_programming_masterclass/section4/MethodOverloading.java
@@ -7,16 +7,23 @@ public class MethodOverloading {
         calculateScore(75);
         calculateScore();
 
+        // Chalenge
+        System.out.println("\n=== Chalenge ===");
+        System.out.println("feet and inches in cm: " + calcFeetAndInchesToCentimeters(6, 1.6));
+        System.out.println("inches in cm: " + calcFeetAndInchesToCentimeters(73.6));
+
     }
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " score " + score + " points");
-        return score*1000;
+        return score * 1000;
     }
+
     public static int calculateScore(int score) {
         System.out.println("Unnamed player score " + score + " points");
-        return score*1000;
+        return score * 1000;
     }
+
     public static int calculateScore() {
         System.out.println("Np player name no player score ");
         return 0;
@@ -25,16 +32,21 @@ public class MethodOverloading {
     // Challenge
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
         if (feet >= 0 && inches >= 0 && inches <= 12) {
-            double centemeters = feet/30.48 + inches/2.54;
-            return centemeters;
+            double cm = (feet * 12 + inches) * 2.54;
+            return cm;
         } else {
             return -1;
         }
     }
+
+    // blad - brakuje 1 cala?! już nie :) wystarczyło zaokrąglić wynik dzielenia stopy na cale,
+    // do liczby całkiwtej czy cast int.
     public static double calcFeetAndInchesToCentimeters(double inches) {
         if (inches >= 0) {
-            double centemeters = inches*2.54;
-            return centemeters;
+            double feet = (int) (inches / 12);
+            inches = inches % 12;
+            return calcFeetAndInchesToCentimeters(feet, inches);
+
         } else {
             return -1;
         }
