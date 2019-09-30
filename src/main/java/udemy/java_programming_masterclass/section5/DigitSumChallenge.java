c0a1aff8e497ef37a64238e613bba3bc895631bb
RS <rafal-stefanski@wp.pl>
2019-09-30 17:49:27 +0200

    Udemy - programming masterclass (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section5/DigitSumChallenge.java
+++ src/main/java/udemy/java_programming_masterclass/section5/DigitSumChallenge.java
@@ -2,26 +2,37 @@ package udemy.java_programming_masterclass.section5;
 
 public class DigitSumChallenge {
     public static void main(String[] args) {
-        int testowaliczba = 123;                         // z 123
-        int wyciagZTestowejProcent = testowaliczba % 10; // wyciaga 3
-        int wyciagZTestowejSlassh = testowaliczba / 10;  // wyciaga 12
+        int testowaliczba = 1;                 // z 123
+        int wyciagZProcent = testowaliczba % 10; // wyciaga 3
+        int wyciagZeSlash = testowaliczba / 10;  // wyciaga 12
 
-        System.out.println(wyciagZTestowejProcent);
-        System.out.println(wyciagZTestowejSlassh);
-        System.out.println("");
-        System.out.println(sumDigits(125));
+        System.out.println(wyciagZProcent);
+        System.out.println(wyciagZeSlash);
+        System.out.println("=== Challenge ===");
+        System.out.println("-> " + sumDigits(125));
     }
 
 
     public static int sumDigits(int number) {
-        if (number < 10) {
-            return -1;
-        }
+        int sum = 0;
+        do {
+            sum = sum + (number % 10);
+        } while (number / 10 == 0);
 
-        int sumOfDigits = 0;
-        for (int i = number % 10; i > 9; i = number / 10) {
-            sumOfDigits += i;
-        }
-        return sumOfDigits;
+
+        //        if (number < 10) {
+//            return -1;
+//        }
+//
+//        do {
+//            sum += number % 10;
+//        } while (number > 9);
+//        return sum;
+
+//        int sumOfDigits = 0;
+//        for (int i = number % 10; i > 9; i = number / 10) {
+//            sumOfDigits += i;
+//        }
+        return sum;
     }
 }
