29c4bc7c5d55c96528f3692cc0bd65da910bb08a
rafal-rs <rafal-stefanski@wp.pl>
2019-10-01 22:24:57 +0200

    #zadania udemy @Ultrabook

--- src/main/java/udemy/java_programming_masterclass/section5/exercise15/NumberPalindrome.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise15/NumberPalindrome.java
@@ -12,20 +12,38 @@ public class NumberPalindrome {
         System.out.println("testowaLiczba % 10 = " + wyciagZProcent +
                 ", testowaLiczba / 10 = " + wyciagZeSlash);
 
+//        System.out.println("***  ***  ***  ***\nisPalindrome(121) should be true and is -> " +
+//                isPalindrome(121) + face(isPalindrome(121)));
+//        System.out.println("isPalindrome(707) should be true and is -> " +
+//                isPalindrome(707) + face(isPalindrome(707)));
+//        System.out.println("isPalindrome(11212) should be true and is -> " +
+//                isPalindrome(11212) + face(isPalindrome(11212)));
+        System.out.println(isPalindrome(11212));
+    }
 
+    private static String face(boolean palindrome) {
+        if (palindrome) {
+            return " :)";
+        } else {
+            return " :(";
+        }
     }
 
     public static boolean isPalindrome(int number) {
 
-        int reverse = 0;
+        int modifiedNumber = number;
+        int reversedNumber = 0;
+        int factor = 10;
 
-        while (number!=0) {
-            reverse = (number % 10);
-//            reverse + reverse;
+        while (modifiedNumber != 0) {
+            int digit = modifiedNumber % 10;
+            modifiedNumber /= 10;
+            reversedNumber += (int) (digit * (factor * 0.1));
+            factor = factor * 10;
         }
 
 
-        if (reverse == number) {
+        if (reversedNumber == number) {
             return true;
         } else {
             return false;
