cf3146b97af1f23580523addc4125c5ac03d1af4
rafal-rs <rafal-stefanski@wp.pl>
2019-10-07 18:57:04 +0200

    #zadania udemy @Ultrabook

--- src/main/java/udemy/java_programming_masterclass/section5/exercise23/NumberToWords.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise23/NumberToWords.java
@@ -1,5 +1,24 @@
 package udemy.java_programming_masterclass.section5.exercise23;
 
 public class NumberToWords {
+//    public static String numberToWords(int number) {
+//        if (number < 0) {
+//            System.out.println("Invalid Value");
+//        }
+//
+//        return number;
+//    }
+
+    public static int reverse(int number) {
+
+        int reversedNumber = 0;
+        for (int i = 0; number/10==0; i++) {
+
+            int lastDigit = number / 10;
+            reversedNumber *= 10;
+            reversedNumber += lastDigit;
+        }
+        return reversedNumber;
+    }
 
 }
