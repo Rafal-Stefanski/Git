0743bd9a11a56f50157d230a61154f63c8627b67
RS <rafal-stefanski@wp.pl>
2019-10-10 17:12:28 +0200

    Udemy - programming masterclass (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section5/exercise24/FlourPacker.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise24/FlourPacker.java
@@ -2,7 +2,16 @@ package udemy.java_programming_masterclass.section5.exercise24;
 
 public class FlourPacker {
     public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
 
+        while (bigCount * 5 + smallCount <= goal) {
+            if (bigCount * 5 <= goal) {
+                return true;
+            }
+            return false;
+        }
 
         return true;
     }
---
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise24/Main.java
@@ -0,0 +1,13 @@
+package udemy.java_programming_masterclass.section5.exercise24;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(FlourPacker.canPack(1,0,4) + " should be 'false'");
+        System.out.println(FlourPacker.canPack(1,0,5) + " should be 'true'");
+        System.out.println(FlourPacker.canPack(0,5,4) + " should be 'true'");
+        System.out.println(FlourPacker.canPack(2,2,11) + " should be 'true'");
+        System.out.println(FlourPacker.canPack(-3,2,12) + " should be 'false'");
+
+
+    }
+}
