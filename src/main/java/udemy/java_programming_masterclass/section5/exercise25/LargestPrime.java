ba58a0d6585ed232e23d63b9786dccfff05c4b3d
rafal-rs <rafal-stefanski@wp.pl>
2019-10-15 23:13:00 +0200

    #zadania udemy @Ultrabook

---
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise25/LargestPrime.java
@@ -0,0 +1,10 @@
+package udemy.java_programming_masterclass.section5.exercise25;
+
+public class LargestPrime {
+    public static int getLargerstPrime(int number) {
+        if (number > 1) {
+
+        }
+        return -1;
+    }
+}
