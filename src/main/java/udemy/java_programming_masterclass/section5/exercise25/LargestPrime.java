11d7fe4cec4a2fe1a18fd0e97bbc0468db901635
RS <rafal-stefanski@wp.pl>
2019-10-17 17:08:49 +0200

    Udemy - programming masterclass (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section5/exercise25/LargestPrime.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise25/LargestPrime.java
@@ -3,7 +3,12 @@ package udemy.java_programming_masterclass.section5.exercise25;
 public class LargestPrime {
     public static int getLargerstPrime(int number) {
         if (number > 1) {
-//            for (int i = number, ,i--)
+            for (int i = number; i >1 ; i--) {
+                if (number % i == 0) {
+
+                }
+            }
+
         }
         return -1;
     }
