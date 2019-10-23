fa9d2d85d5cc77e008f78034250c20b4ed7cb908
rafal-rs <rafal-stefanski@wp.pl>
2019-10-23 22:28:43 +0200

    #zadania udemy @Ultrabook

--- src/main/java/udemy/java_programming_masterclass/section5/exercise19/LastDigitChecker.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise19/LastDigitChecker.java
@@ -1,6 +1,6 @@
 package udemy.java_programming_masterclass.section5.exercise19;
 
-public class LastDigitChecke {
+public class LastDigitChecker {
     public static boolean hasSameLastDigit(int no_1, int no_2, int no_3) {
         while (isValid(no_1) && isValid(no_2) && isValid(no_3)) {
             if (no_1 % 10 == no_2 % 10 || no_2 % 10 == no_3 % 10 || no_1 % 10 == no_3 % 10) {
--- src/main/java/udemy/java_programming_masterclass/section5/exercise19/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise19/Main.java
@@ -2,9 +2,9 @@ package udemy.java_programming_masterclass.section5.exercise19;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LastDigitChecke.hasSameLastDigit(41, 22, 71) + " - should return 'true'");
-        System.out.println(LastDigitChecke.hasSameLastDigit(23, 32, 42) + " - should return 'true'");
-        System.out.println(LastDigitChecke.hasSameLastDigit(9, 99, 999) + " - should return 'false'");
-        System.out.println(LastDigitChecke.hasSameLastDigit(22, 23, 34) + " - should return 'false'");
+        System.out.println(LastDigitChecker.hasSameLastDigit(41, 22, 71) + " - should return 'true'");
+        System.out.println(LastDigitChecker.hasSameLastDigit(23, 32, 42) + " - should return 'true'");
+        System.out.println(LastDigitChecker.hasSameLastDigit(9, 99, 999) + " - should return 'false'");
+        System.out.println(LastDigitChecker.hasSameLastDigit(22, 23, 34) + " - should return 'false'");
     }
 }
