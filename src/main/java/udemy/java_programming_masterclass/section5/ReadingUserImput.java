d6c6acd1b6f2c554890cc8c78abda1cf33daf131
Rafał Stefański <rafal-stefanski@wp.pl>
2019-10-27 17:33:36 +0100

    Udemy - programming masterclass (@MSI)

---
+++ src/main/java/udemy/java_programming_masterclass/section5/ReadingUserImput.java
@@ -0,0 +1,17 @@
+package udemy.java_programming_masterclass.section5;
+
+import java.util.Scanner;
+
+public class ReadingUserImput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is " + name);
+
+        scanner.close();
+
+    }
+}
--- src/main/java/udemy/java_programming_masterclass/section5/exercise26/DiagonalStar.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise26/DiagonalStar.java
@@ -2,15 +2,19 @@ package udemy.java_programming_masterclass.section5.exercise26;
 
 public class DiagonalStar {
     public static void printSquareStar(int number) {
-        if (number < 5) {
+        if (number >= 5) {
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j <= number; j++) {
+                    if (i == 1 || i == number || j == 1 || j == number || i == j || j == number - i + 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        } else {
             System.out.println("Invalid Value");
         }
-
-        for (int i = number; i >= 5; i--) {
-            System.out.print("*");
-            continue;
-        }
-
     }
-
 }
