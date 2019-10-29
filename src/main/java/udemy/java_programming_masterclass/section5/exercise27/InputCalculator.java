6e9e4ab7112abbc04b9cbd1b8cdd531878836a55
Rafał Stefański <rafal-stefanski@wp.pl>
2019-10-29 16:21:43 +0100

    Udemy - programming masterclass (@MSI)

---
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise27/InputCalculator.java
@@ -0,0 +1,14 @@
+package udemy.java_programming_masterclass.section5.exercise27;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+
+
+
+    }
+}
---
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise27/Main.java
@@ -0,0 +1,7 @@
+package udemy.java_programming_masterclass.section5.exercise27;
+
+public class Main {
+    public static void main(String[] args) {
+        InputCalculator.inputThenPrintSumAndAverage();
+    }
+}
