f07195d9b4a7b99515fb8d31d7f62e0ac48d9045
Rafał Stefański <rafal-stefanski@wp.pl>
2019-10-30 17:43:39 +0100

    Udemy - programming masterclass (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section5/exercise28/PaintJob.java
+++ src/main/java/udemy/java_programming_masterclass/section5/exercise28/PaintJob.java
@@ -28,3 +28,21 @@ public class PaintJob {
         return (int) Math.ceil(bucketCount);
     }
 }
+
+// Buchalka solution
+
+//    public static int getBucketCount(double area, double areaPerBucket) {
+//        return getBucketCount(1,area,areaPerBucket,0);
+//    }
+//    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+//        if (Math.min(width,Math.min(height,areaPerBucket))<=0||extraBuckets<0){
+//            return -1;
+//        }
+//        return (int)Math.ceil((width*height)/areaPerBucket-extraBuckets);
+//    }
+//
+//    public static int getBucketCount(double width, double height, double areaPerBucket){
+//        return getBucketCount(width,height,areaPerBucket,0);
+//    }
+
+
