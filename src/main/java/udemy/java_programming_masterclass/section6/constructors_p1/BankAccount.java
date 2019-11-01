7d991f02f362b886c9d96925ebe1f7f0dd7f6112
Rafał Stefański <rafal-stefanski@wp.pl>
2019-11-01 16:16:28 +0100

    Udemy - programming masterclass (@MSI)

---
+++ src/main/java/udemy/java_programming_masterclass/section6/constructors_p1/BankAccount.java
@@ -0,0 +1,11 @@
+package udemy.java_programming_masterclass.section6.constructors_p1;
+
+public class BankAccount {
+    public String accountNumber;
+    public Double balance;
+    public String customerName;
+    public String customerEmail;
+    public String customerPhoneNumber;
+
+
+}
