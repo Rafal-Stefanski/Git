e3ea564d92f6ae22b918bccd68c7ce1bd94a71bf
rafal-rs <rafal-stefanski@wp.pl>
2019-11-19 21:29:22 +0100

    #zadania udemy @Ultrabook

--- src/main/java/udemy/java_programming_masterclass/section6/inheritance_p1/challenge/Convertible.java
+++
@@ -1,9 +0,0 @@
-package udemy.java_programming_masterclass.section6.inheritance_p1.challenge;
-
-public class Convertible extends Car {
-    private boolean retractableRoof;
-
-    public Convertible(boolean openRoof) {
-        this.retractableRoof = openRoof;
-    }
-}
