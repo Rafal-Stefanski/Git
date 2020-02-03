11135fcb2c6d98d901b9fcdecabee722aeac9287
Rafał Stefański <rafal-stefanski@wp.pl>
2020-02-03 22:20:08 +0100

    Programming Masterclass (@T) - section 8 Arrays.

--- src/main/java/udemy/java_programming_masterclass/section8/challenge_autoboxing_unboxing/solution/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section8/challenge_autoboxing_unboxing/solution/Main.java
@@ -1,8 +1,31 @@
 package udemy.java_programming_masterclass.section8.challenge_autoboxing_unboxing.solution;
 
+/*
+    Niestety kod nie działa. Do przejrzenia.
+ */
+
 public class Main {
     public static void main(String[] args) {
+        Bank bank = new Bank("Australia Bank");
 
+        bank.addBranch("Adelaide");
+
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Mike", 175.34);
+        bank.addCustomer("Adelaide", "Perry", 220.12);
+
+        bank.addBranch("Sydney");
+
+        bank.addCustomerTransaction("Adelaide", "Tim", 50.05);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+        bank.addCustomerTransaction("Adelaide", "Perry", 1.65);
+
+        bank.listCustomers("Adelaide", true);
+        bank.listCustomers("Sydney", true);
+
+        if (!bank.addCustomer("Melbourne", "Brian", 5.53)) {
+            System.out.println("Error Melbourne branch does not exist");
+        }
     }
 }
 
