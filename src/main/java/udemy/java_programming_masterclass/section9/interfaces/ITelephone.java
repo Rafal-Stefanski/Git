ef7b3fc8f9456db0c5485dbaf09b91e3f7491c76
Rafał Stefański <rafal-stefanski@wp.pl>
2020-02-15 16:12:11 +0100

    Programming Masterclass (@M) section 9 arrays.

---
+++ src/main/java/udemy/java_programming_masterclass/section9/interfaces/ITelephone.java
@@ -0,0 +1,10 @@
+package udemy.java_programming_masterclass.section9.interfaces;
+
+public interface ITelephone {
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+
+}
