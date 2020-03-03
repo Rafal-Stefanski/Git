027885beb79af801e698ec8f63c79e91eec1417b
Rafał Stefański <rafal-stefanski@wp.pl>
2020-03-03 21:38:15 +0100

    Programming Masterclass (@T) - section 8 Arrays.

--- src/main/java/udemy/java_programming_masterclass/section7/polimorphism/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section7/polimorphism/Main.java
@@ -22,11 +22,12 @@ class Jaws extends Movie {
     }
 
     public String plot() {
-        return "A shark eats a lot of people";
+        return "A shark eats lots of people";
     }
 }
 
 class IndependenceDay extends Movie {
+
     public IndependenceDay() {
         super("Independence Day");
     }
@@ -38,6 +39,7 @@ class IndependenceDay extends Movie {
 }
 
 class MazeRunner extends Movie {
+
     public MazeRunner() {
         super("Maze Runner");
     }
@@ -55,7 +57,7 @@ class StarWars extends Movie {
 
     @Override
     public String plot() {
-        return "Imperial forces try to take over the universe";
+        return "Imperial Forces try to take over the universe";
     }
 }
 
@@ -63,22 +65,24 @@ class Forgetable extends Movie {
     public Forgetable() {
         super("Forgetable");
     }
-    // No plot method here.
+
+    // No plot method
 }
 
 
 public class Main {
-    public static void main(String[] args) {
-        Movie movie = randomMovie();
 
-        for (int i = 1; i < 11; i++) {
-            System.out.println("Movie #" + i + " : " + movie.getName() + "\n"
-                    + "Plot: " + movie.plot() + "\n");
+    public static void main(String[] args) {
+        for(int i=1; i<11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i +
+                    " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
         }
     }
 
     public static Movie randomMovie() {
-        int randomNumber = (int) (Math.random() * 5) + 1;
+        int randomNumber = (int) (Math.random() * 5) +1;
         System.out.println("Random number generated was: " + randomNumber);
         switch (randomNumber) {
             case 1:
@@ -92,6 +96,7 @@ public class Main {
             case 5:
                 return new Forgetable();
         }
+
         return null;
     }
 }
