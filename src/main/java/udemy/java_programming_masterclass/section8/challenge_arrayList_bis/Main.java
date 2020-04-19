4f0ec29777cc6433584815ee845d40d55c2bf297
Rafał Stefański <rafal-stefanski@wp.pl>
2020-04-19 23:47:04 +0200

    Programming Masterclass (@T) - section 8 review.

--- src/main/java/udemy/java_programming_masterclass/section8/challenge_arrayList_bis/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section8/challenge_arrayList_bis/Main.java
@@ -1,8 +1,28 @@
 package udemy.java_programming_masterclass.section8.challenge_arrayList_bis;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("0039 330 4404");
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while (quit) {
+
+        }
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions() {
+        System.out.println("\nAvaliable actions:\npres");
+        System.out.println("1 - to shutdown\n");
     }
 }
 
