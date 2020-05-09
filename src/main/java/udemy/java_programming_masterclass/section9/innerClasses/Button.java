db24e5511cb68b79144eaf9421a70fff812f751a
Rafał Stefański <rafal-stefanski@wp.pl>
2020-05-09 16:19:18 +0200

    Programming Masterclass (@M) section 9.

---
+++ src/main/java/udemy/java_programming_masterclass/section9/innerClasses/Button.java
@@ -0,0 +1,26 @@
+package udemy.java_programming_masterclass.section9.innerClasses;
+
+public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+}
