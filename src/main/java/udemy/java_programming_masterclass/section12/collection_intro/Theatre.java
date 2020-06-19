0f058ec6540580a3d5d335bba74ee260996428ee
Rafał Stefański <rafal-stefanski@wp.pl>
2020-06-19 18:31:06 +0200

    Programming Masterclass (@M) section 12.

--- src/main/java/udemy/java_programming_masterclass/section12/collection_intro/Theatre.java
+++ src/main/java/udemy/java_programming_masterclass/section12/collection_intro/Theatre.java
@@ -4,15 +4,40 @@ import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    public List <Seat> seats = new ArrayList<>();
+    private List<Seat> seats = new ArrayList<>();
+
+    static final Comparator<Seat> PRICE_ORDER;
+
+    static {
+        PRICE_ORDER = new Comparator<Seat>() {
+            @Override
+            public int compare(Seat seat1, Seat seat2) {
+                if (seat1.getPrice() < seat2.getPrice()) {
+                    return -1;
+                } else if (seat1.getPrice() > seat2.getPrice()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+    }
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
 
-        int lastRow = 'A' + (numRows - 1);
+        int lastRow = 'A' + (numRows -1);
         for (char row = 'A'; row <= lastRow; row++) {
-            for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNum));
+            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+                double price = 12.00;
+
+                if((row < 'D') && (seatNum >=4 && seatNum <=9)) {
+                    price = 14.00;
+                } else if((row > 'F') || (seatNum < 4 || seatNum > 9)) {
+                    price = 7.00;
+                }
+
+                Seat seat = new Seat(row + String.format("%02d", seatNum), price);
                 seats.add(seat);
             }
         }
@@ -22,8 +47,8 @@ public class Theatre {
         return theatreName;
     }
 
-    public boolean reservedSeat(String seatNumber) {
-        Seat requestedSeat = new Seat(seatNumber);
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = new Seat(seatNumber, 0);
         int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
         if (foundSeat >= 0) {
             return seats.get(foundSeat).reserve();
@@ -31,35 +56,21 @@ public class Theatre {
             System.out.println("There is no seat " + seatNumber);
             return false;
         }
-//        for (Seat seat : seats) {
-//            System.out.print(".");
-//            if (seat.getSeatNumber().equals(seatNumber)) {
-//                requestedSeat = seat;
-//                break;
-//            }
-//        }
-//
-//        if (requestedSeat == null) {
-//            System.out.println("There is no seat " + seatNumber);
-//            return false;
-//        }
-//
-//        return requestedSeat.reserve();
     }
 
     // for testing
-    public void getSeat() {
-        for (Seat seat : seats) {
-            System.out.println(seat.getSeatNumber());
-        }
+    public Collection<Seat> getSeats() {
+        return seats;
     }
 
     public class Seat implements Comparable<Seat> {
         private final String seatNumber;
+        private double price;
         private boolean reserved = false;
 
-        public Seat(String seatNumber) {
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
         }
 
         @Override
@@ -68,7 +79,7 @@ public class Theatre {
         }
 
         public boolean reserve() {
-            if (!this.reserved) {
+            if(!this.reserved) {
                 this.reserved = true;
                 System.out.println("Seat " + seatNumber + " reserved");
                 return true;
@@ -78,9 +89,9 @@ public class Theatre {
         }
 
         public boolean cancel() {
-            if (this.reserved) {
+            if(this.reserved) {
                 this.reserved = false;
-                System.out.println("reservation of seat " + seatNumber + " cancelled");
+                System.out.println("Reservation of seat " + seatNumber + " cancelled");
                 return true;
             } else {
                 return false;
@@ -90,5 +101,11 @@ public class Theatre {
         public String getSeatNumber() {
             return seatNumber;
         }
+
+        public double getPrice() {
+            return price;
+        }
     }
+
 }
+
