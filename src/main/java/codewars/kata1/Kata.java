50d2a5112a201a8561ede3d85ec17a8e4857f7cf
Rafał Stefański <rafal-stefanski@wp.pl>
2020-08-21 19:00:03 +0200

    Codewars package added.

---
+++ src/main/java/codewars/kata1/Kata.java
@@ -0,0 +1,16 @@
+package codewars.kata1;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        // Your code here!
+//     String number = "(123) 456-7890";
+//     return number;
+//     int [] number = new int [];
+//     number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        String number = Arrays.toString(numbers);
+        return number;
+    };
+}
