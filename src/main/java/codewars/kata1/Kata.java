fb832051b8276ab1fdc794b51209dac4ac46243a
Rafał Stefański <rafal-stefanski@wp.pl>
2020-08-22 11:17:11 +0200

    codewars Kata (@T)

--- src/main/java/codewars/kata1/Kata.java
+++ src/main/java/codewars/kata1/Kata.java
@@ -3,14 +3,39 @@ package codewars.kata1;
 import java.util.Arrays;
 
 public class Kata {
+
+    public static String phoneNo(int[] numbers) {
+        String openBraces = "(";
+        String firstPartOfNumber;
+
+        System.out.print("(");
+        for (int i = 0; i < 3; i++) {
+//            Arrays.toString.numbers[i];
+        }
+        System.out.print(") ");
+        for (int j = 3; j < 6; j++) {
+            System.out.print(numbers[j]);
+        }
+        System.out.print("-");
+        for (int j = 6; j <= 9; j++) {
+            System.out.print(numbers[j]);
+        }
+
+        return null;
+    }
+
+
     public static String createPhoneNumber(int[] numbers) {
         // Your code here!
 //     String number = "(123) 456-7890";
 //     return number;
 //     int [] number = new int [];
 //     number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+//        "(123) 456-7890"
 
-        String number = Arrays.toString(numbers);
-        return number;
-    };
+//        String phoneNumber = toString(numbers);
+
+        return null;
+
+    }
 }
