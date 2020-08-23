40458a640cdd285899a2373fd1c4ad781fac72e5
Rafał Stefański <rafal-stefanski@wp.pl>
2020-08-23 23:11:52 +0200

    codewars Kata 3 (@T)

---
+++ src/main/java/codewars/kata3descendingOrder/DescendingOrder.java
@@ -0,0 +1,25 @@
+package codewars.kata3descendingOrder;
+
+import java.util.ArrayList;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        //Your code
+
+        int minNumber = num;
+        while (num != 0) {
+
+        }
+//        ArrayList<Integer> array = new ArrayList<Integer>();
+//        do{
+//            array.add(temp % 10);
+//            temp /= 10;
+//        } while  (temp > 0);
+//        int[] array = new int[3];
+
+
+
+
+        return -1;
+    }
+}
