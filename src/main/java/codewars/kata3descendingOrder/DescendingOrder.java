fd5cb9bf61875898522703efc40cfc57bb2bca08
Rafał Stefański <rafal-stefanski@wp.pl>
2020-08-27 23:15:14 +0200

    codewars Kata 3 (@T)

--- src/main/java/codewars/kata3descendingOrder/DescendingOrder.java
+++ src/main/java/codewars/kata3descendingOrder/DescendingOrder.java
@@ -6,20 +6,36 @@ public class DescendingOrder {
     public static int sortDesc(final int num) {
         //Your code
 
-        int minNumber = num;
-        while (num != 0) {
+        // niestety wciąż nie działa ;(
 
+        if (num > 0) {
+
+            int temp = num;
+            ArrayList<Integer> array = new ArrayList<Integer>();
+            do{
+                array.add(temp % 10);
+                temp /= 10;
+            } while  (temp > 0);
+
+            return DescendingOrder.toInt(array);
+        } else {
+            return 0;
         }
-//        ArrayList<Integer> array = new ArrayList<Integer>();
-//        do{
-//            array.add(temp % 10);
-//            temp /= 10;
-//        } while  (temp > 0);
-//        int[] array = new int[3];
 
+    }
 
+    public static int toInt(ArrayList<Integer> array) {
+        int result = 0;
+        int offset = 1;
+        for(int i = array.toArray().length - 1; i >= 0; i--) {
+            result += array.get(i) *offset;
+            offset *= 10;
+        }
+        return result;
+    }
 
-
-        return -1;
+    public static void main(String[] args) {
+        int number = sortDesc(123);
+        System.out.println(number);
     }
 }
