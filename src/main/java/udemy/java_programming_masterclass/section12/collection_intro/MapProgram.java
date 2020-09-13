a42455adfb306cd306caa4b9ebe333521bb73e6d
Rafał Stefański <rafal-stefanski@wp.pl>
2020-09-13 15:39:49 +0200

    Collections update (@T)

--- src/main/java/udemy/java_programming_masterclass/section12/collection_intro/MapProgram.java
+++ src/main/java/udemy/java_programming_masterclass/section12/collection_intro/MapProgram.java
@@ -1,17 +1,28 @@
 package udemy.java_programming_masterclass.section12.collection_intro;
 
-import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapProgram {
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
-        languages.put("Java", "A compiled high level, object-orientated, platform independent language");
+        if (languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java", "A compiled high level, object-orientated, platform independent language");
+            System.out.println("Java added successfully");
+        }
+
         languages.put("Python", "an interpreted, object-oriented, high level programming language with dynamic semantics");
         languages.put("Algol", "an algorithmic language");
-        languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code");
-        languages.put("Lisp", " Therein lies madness");
+        System.out.println(languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
+        if (languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java", "this course is about Java");
+        }
+
 
         System.out.println(languages.get("Java"));
         languages.put("Java", "this course is about Java");
@@ -20,7 +31,7 @@ public class MapProgram {
         System.out.println("============ ============ ==========");
 
         for (String key : languages.keySet()) {
-            System.out.println(key+" : " + languages.get(key));
+            System.out.println(key + " : " + languages.get(key));
         }
     }
 }
