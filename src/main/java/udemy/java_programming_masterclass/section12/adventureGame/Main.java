21fe4b6652f608bc49b047430684013b98365b37
Rafał Stefański <rafal-stefanski@wp.pl>
2020-09-14 16:23:28 +0200

    Collection Adventure Game example (@MSI).

--- src/main/java/udemy/java_programming_masterclass/section12/adventureGame/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section12/adventureGame/Main.java
@@ -5,7 +5,8 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private final static Map<Integer, Location> locations = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -36,30 +37,46 @@ public class Main {
         locations.get(5).addExit("W", 2);
         locations.get(5).addExit("Q", 0);
 
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
         int loc = 1;
-        while(true) {
+        while (true) {
             System.out.println(locations.get(loc).getDescription());
-            if (loc == 0){
+            if (loc == 0) {
                 break;
             }
 
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.print("\nAvailable exits are ");
-            for (String exit: exits.keySet()) {
+            for (String exit : exits.keySet()) {
                 System.out.print(exit + ", ");
             }
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
 
-            if(exits.containsKey(direction)) {
+            if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
             } else {
                 System.out.println("You cannot go in that direction");
             }
         }
 
-
+//      Challenge
         // Change the program to allow players to type full words, or phrases, then move to the
         // correct location based upon their input.
         // The player should be able to type commands such as "Go West", "run South", or just "East"
