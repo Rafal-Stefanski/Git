5d0f26f1334657383bac193ebeae9b4f5f378a1c
Rafał Stefański <rafal-stefanski@wp.pl>
2020-09-24 22:30:07 +0200

    Crypto Kata from CodeWars new ideas (@T)

--- src/main/java/codewars/kata6Cryptanalysis/Crypto.java
+++ src/main/java/codewars/kata6Cryptanalysis/Crypto.java
@@ -1,10 +1,14 @@
 package codewars.kata6Cryptanalysis;
 
-public class Crypto {
-    public static String wordPattern(final String word) {
-        // Your code here!
+import java.lang.reflect.Array;
 
-        String[] letters = word.split(".");
+public class Crypto {
+    public static String wordPattern(String word) {
+        // Your code here!
+//        word="ala";
+        char[] forCountingArray = word.toCharArray();
+
+        String[] letters = word.split("");
         int letterNumber = 0;
         for (int i = 0; i < word.length(); i++) {
             for (int j = 0; j < word.length(); i++) {
@@ -12,11 +16,42 @@ public class Crypto {
             }
         }
 
-
+        String finalString = forCountingArray.toString();
         return null;
     }
 
     public static void main(String[] args) {
+
+        //Initialize the String which needs to be split
+        String str = "Enlighter";
+
+        //Use the Split method and store the array of Strings returned in a String array.
+        String[] arr = str.split("");
+        System.out.println();
+        //Printing the characters using for-each loop
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("\nFrom String: " + str + "\n");
+
+
+        //Initialize a test String
+        String test = "Hello World, CodeSpeedy here";
+        //Store the length in a integer variable
+        int length = test.length();
+
+        //Character array to store the characters
+        //of length same as the string = 'length'
+        char[] arrOfChar = new char[test.length()];
+
+        //loop to store each character in the array and then print the same
+        for (int i=0; i<test.length(); i++)
+        {
+            arrOfChar[i] = test.charAt(i);
+            System.out.print(arrOfChar[i] + " ");
+        }
+        System.out.println("\n\nFrom String: " + test);
 
     }
 }
