a1d44507a35515c2935291cd146098a9df7308fa
Rafał Stefański <rafal-stefanski@wp.pl>
2020-10-08 17:29:42 +0200

    I/O Locations (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
+++ src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
@@ -1,15 +1,37 @@
 package udemy.java_programming_masterclass.section14.introductionToIO;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created by timbuchalka on 2/04/2016.
- */
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("locations.txt");
+            for (Location location : locations.values()) {
+                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("In catch block");
+            e.printStackTrace();
+        } finally {
+            System.out.println("In finally clock");
+            try {
+                if (locFile != null) {
+                    System.out.println("Attempting to close locfile");
+                    locFile.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     static {
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
