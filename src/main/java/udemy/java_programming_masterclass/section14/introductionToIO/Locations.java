ccc24e6fba9db5d979d0bddcea3086b216c0327c
Rafał Stefański <rafal-stefanski@wp.pl>
2020-10-09 17:31:41 +0200

    I/O Locations (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
+++ src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
@@ -10,59 +10,64 @@ import java.util.Set;
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
-    public static void main(String[] args) {
-        FileWriter locFile = null;
-        try {
-            locFile = new FileWriter("locations.txt");
+    public static void main(String[] args) throws IOException {
+        try (FileWriter locFile = new FileWriter("locations.txt");
+             FileWriter dirFile = new FileWriter("directions.txt")) {
             for (Location location : locations.values()) {
                 locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-            }
-        } catch (IOException e) {
-            System.out.println("In catch block");
-            e.printStackTrace();
-        } finally {
-            System.out.println("In finally clock");
-            try {
-                if (locFile != null) {
-                    System.out.println("Attempting to close locfile");
-                    locFile.close();
+                for (String direction : location.getExits().keySet()) {
+                    dirFile.write(location.getLocationID() + "," + direction + location.getExits().get(direction) + "\n");
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
+
+//        FileWriter locFile = null;
+//        try {
+//            locFile = new FileWriter("locations.txt");
+//            for (Location location : locations.values()) {
+//                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+////                throw new IOException("test exception throw while writing"); //   CAUTION: line only for testing remember to delete, or comment after testing!
+//            }
+//        } finally {
+//            System.out.println("In finally clock");
+//            if (locFile != null) {
+//                System.out.println("Attempting to close locfile");
+//                locFile.close();
+//            }
+//        }
     }
 
     static {
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 2);
         tempExit.put("E", 3);
         tempExit.put("S", 4);
         tempExit.put("N", 5);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("N", 1);
         tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("S", 1);
         tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest",tempExit));
+        locations.put(5, new Location(5, "You are in the forest", tempExit));
 
     }
+
     @Override
     public int size() {
         return locations.size();
