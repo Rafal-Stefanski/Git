b48f84084983724b33da88bcc0aa03b07edd73b3
Rafał Stefański <rafal-stefanski@wp.pl>
2020-10-14 18:42:36 +0200

    I/O Stream (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
+++ src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
@@ -11,13 +11,18 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 
     public static void main(String[] args) throws IOException {
-        try (BufferedWriter locFile = new BufferedWriter(new FileWriter("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/locations.txt"));
-             BufferedWriter dirFile = new BufferedWriter(new FileWriter("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/directions.txt"))) {
+        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/locations.dat")));) {
             for (Location location : locations.values()) {
-                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+                locFile.writeInt(location.getLocationID());
+                locFile.writeUTF(location.getDescription());
+                System.out.println("Writing location " + location.getLocationID() + " : " + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() - 1) + " exits.");
+                locFile.writeInt(location.getExits().size() - 1);
                 for (String direction : location.getExits().keySet()) {
                     if (!direction.equalsIgnoreCase("Q")) {
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
+                        locFile.writeUTF(direction);
+                        locFile.writeInt(location.getExits().get(direction));
                     }
                 }
             }
