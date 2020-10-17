025531edb23964cab67cca1be4a663b649d4fbb8
Rafał Stefański <rafal-stefanski@wp.pl>
2020-10-17 18:33:33 +0200

    Object input Stream Serialization and RandomAccessFile (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
+++ src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/Locations.java
@@ -11,22 +11,6 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 
     public static void main(String[] args) throws IOException {
-//        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/locations.dat")));) {
-//            for (Location location : locations.values()) {
-//                locFile.writeInt(location.getLocationID());
-//                locFile.writeUTF(location.getDescription());
-//                System.out.println("Writing location " + location.getLocationID() + " : " + location.getDescription());
-//                System.out.println("Writing " + (location.getExits().size() - 1) + " exits.");
-//                locFile.writeInt(location.getExits().size() - 1);
-//                for (String direction : location.getExits().keySet()) {
-//                    if (!direction.equalsIgnoreCase("Q")) {
-//                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
-//                        locFile.writeUTF(direction);
-//                        locFile.writeInt(location.getExits().get(direction));
-//                    }
-//                }
-//            }
-//        }
 
         try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/locations.dat")));) {
             for (Location location : locations.values()) {
@@ -49,66 +33,16 @@ public class Locations implements Map<Integer, Location> {
                     locations.put(location.getLocationID(), location);
                 } catch (EOFException e) {
                     eof = true;
-                } catch (ClassNotFoundException e) {
-                    System.out.println("ClassNotFoundException " + e.getMessage());
                 }
             }
-
-//            while (!eof) {
-//                try {
-//                    Map<String, Integer> exits = new LinkedHashMap<>();
-//                    int locID = locFile.readInt();
-//                    String description = locFile.readUTF();
-//                    int numExits = locFile.readInt();
-//                    System.out.println("Read location " + locID + " : " + description);
-//                    System.out.println("Found " + numExits + " exits");
-//                    for (int i = 0; i < numExits; i++) {
-//                        String direction = locFile.readUTF();
-//                        int destination = locFile.readInt();
-//                        exits.put(direction, destination);
-//                        System.out.println("\t\t" + direction + destination);
-//                    }
-//                    locations.put(locID, new Location(locID, description, exits));
-//
-//                } catch (EOFException e) {
-//                    eof = true;
-//                }
-//            }
-        } catch (IOException ioException) {
-            System.out.println("IO Exception");
+        } catch (InvalidClassException e) {
+            System.out.println("InvalidClassException " + e.getMessage());
+        } catch (IOException io) {
+            System.out.println("IO Exception" + io.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException " + e.getMessage());
         }
 
-//        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/locations_big.txt")))) {
-//            scanner.useDelimiter(",");
-//            while (scanner.hasNextLine()) {
-//                int loc = scanner.nextInt();
-//                scanner.skip(scanner.delimiter());
-//                String description = scanner.nextLine();
-//                System.out.println("Imported loc: " + loc + ": " + description);
-//                Map<String, Integer> tempExit = new HashMap<>();
-//                locations.put(loc, new Location(loc, description, tempExit));
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        // Now read the exits
-//        try (BufferedReader dirFile = new BufferedReader(new FileReader("src/main/java/udemy/java_programming_masterclass/section14/introductionToIO/directions_big.txt"))) {
-//            String input;
-//            while ((input = dirFile.readLine()) != null) {
-//                String[] data = input.split(",");
-//                int loc = Integer.parseInt(data[0]);
-//                String direction = data[1];
-//                int destination = Integer.parseInt(data[2]);
-//
-//                System.out.println(loc + ": " + direction + ": " + destination);
-//                Location location = locations.get(loc);
-//                location.addExit(direction, destination);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
