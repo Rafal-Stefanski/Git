d1adc7af7e2d5c0497bfa40449d5576ab93a79c7
Rafał Stefański <rafal-stefanski@wp.pl>
2020-11-16 13:47:09 +0100

    266. Interrupt and Join (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section15/threads/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section15/threads/Main.java
@@ -20,10 +20,18 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run().");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED+"AnotherThread terminated, or timed out, so I'm running again.");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted.");
+                }
             }
         });
 
         myRunnableThread.start();
+//        anotherThread.interrupt();    //  This will stop waiting (for 3 seconds) in AnotherThread.
+
         System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
 
     }
