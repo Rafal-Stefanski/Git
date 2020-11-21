68a82493bb56a7afaeddde982ca130919c057bd3
Rafał Stefański <rafal-stefanski@wp.pl>
2020-11-21 22:25:51 +0100

    282. Live Lock Example and Slipped Conditions (@T)

--- src/main/java/udemy/java_programming_masterclass/section15/politeWorker/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section15/politeWorker/Main.java
@@ -5,5 +5,21 @@ public class Main {
         final Worker worker1 = new Worker("Worker 1", true);
         final Worker worker2 = new Worker("Worker 2", true);
 
+        final ShareResource shareResource = new ShareResource(worker1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                worker1.work(shareResource, worker2);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                worker2.work(shareResource, worker1);
+            }
+        }).start();
+
     }
 }
