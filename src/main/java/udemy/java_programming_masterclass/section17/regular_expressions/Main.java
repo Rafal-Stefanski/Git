3bfb0f0139389cc068b366c77053db55b1b75462
Rafał Stefański <rafal-stefanski@wp.pl>
2020-11-26 16:15:00 +0100

    306. Character classes and Boundary Matchers (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section17/regular_expressions/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section17/regular_expressions/Main.java
@@ -26,5 +26,27 @@ public class Main {
         System.out.println(alphanumeric.replaceAll("[aei]", "I replace a letter here."));
         System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
 
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+
+        String newAphanumeric = "abcDeeeF12Ghhiiiiiijkl99z";
+        System.out.println(newAphanumeric.replaceAll("[^ej]", "X"));
+        System.out.println(newAphanumeric.replaceAll("[abcdef34678]", "X"));
+        System.out.println(newAphanumeric.replaceAll("[a-fA-F3-8]", "X"));
+        System.out.println(newAphanumeric.replaceAll("(?i)[a-f3-8]", "X"));     // (?i) for unicode - no lower upper case sensitivity, for asci (?iu)
+        System.out.println(newAphanumeric.replaceAll("[0-9]", "X"));    // replacing all digits with X
+        System.out.println(newAphanumeric.replaceAll("[\\d]", "X"));    // replacing all digits with X
+        System.out.println(newAphanumeric.replaceAll("[\\D]", "X"));    // replacing all non digits with X
+
+        System.out.println("=============================================");
+        String hasWhitespace = "I have blanks and\ta tab, and also a newline\n";
+        System.out.println(hasWhitespace);
+        System.out.println(hasWhitespace.replaceAll("\\s", ""));    // removes all spaces, tabs and new line.
+        System.out.println(hasWhitespace.replaceAll("\t", "X"));    // replace Tab with an X
+        System.out.println(hasWhitespace.replaceAll("\\S", ""));    // removes all non spaces, tabs and new line, letters removed remains only spaces and tabs.
+        System.out.println(newAphanumeric.replaceAll("\\w", "X"));  // replaces all characters except for whitespaces with an X
+        System.out.println(hasWhitespace.replaceAll("\\w", "X"));   // replaces all characters except for whitespaces with an X
+        System.out.println(hasWhitespace.replaceAll("\\b", "X"));   // surrounds each word with X in the beginning and in the end of each world
+
+
     }
 }
