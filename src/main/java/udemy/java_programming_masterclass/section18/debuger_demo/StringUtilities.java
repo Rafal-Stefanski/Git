16ec8b7d917c23ae5928428648dc873c7e177989
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-02 18:05:01 +0100

    316 Field Watch Points (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section18/debuger_demo/StringUtilities.java
+++ src/main/java/udemy/java_programming_masterclass/section18/debuger_demo/StringUtilities.java
@@ -5,7 +5,16 @@ public class StringUtilities {
     private int charAdded = 0;
 
     public void addChar(StringBuilder sBuilder, char c) {
-        this.sBuilder.append(c);
+        sBuilder.append(c);
         charAdded++;
+    }
+
+    public String upperAndPrefix(String str) {
+        String upper = str.toUpperCase();
+        return "Prefix_ " + upper;
+    }
+
+    public String addSuffix(String str) {
+        return str + "__Suffix";
     }
 }
