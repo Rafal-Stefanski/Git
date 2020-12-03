a21925a09d3d61fc2544080954d35d5fb9445ad0
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-03 22:30:40 +0100

    318. Introduction to Unit Testing with JUnit (@T)

---
+++ src/test/java/udemy/java_programming_masterclass/section18/unit_testing/BankAccountTest.java
@@ -0,0 +1,24 @@
+package udemy.java_programming_masterclass.section18.unit_testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @Test
+    void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @Test
+    void withdraw() {
+        fail("This test has yet to be implemented");
+    }
+
+    @Test
+    void getBalance() {
+        fail("This test has yet to be implemented");
+    }
+
+}
