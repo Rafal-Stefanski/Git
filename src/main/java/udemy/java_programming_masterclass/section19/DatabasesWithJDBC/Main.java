6f518e7e6870029b265ec1117a3647668ba521ea
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-22 18:27:10 +0100

    339. JDBC Insert, Update, Delete (@MSI)

--- src/main/java/udemy/java_programming_masterclass/section19/DatabasesWithJDBC/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section19/DatabasesWithJDBC/Main.java
@@ -1,9 +1,6 @@
 package udemy.java_programming_masterclass.section19.DatabasesWithJDBC;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +9,31 @@ public class Main {
 //            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\RS\\IdeaProjects\\Git\\src\\main\\java\\udemy\\java_programming_masterclass\\section19\\DatabasesWithJDBC\\testjava.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
+                    " (name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Joe', 45632, 'joe@email.com')");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Jane', 4829484, 'jane@somewhere.com')");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Fido', 9038, 'dog@email.com')");
+
+//            statement.execute("UPDATE contacts SET phone = 5566789 WHERE name = 'Jane' ");
+//            statement.execute("DELETE FROM contacts WHERE name = 'Joe'");
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()) {
+                System.out.println(results.getString("name") + " " +
+                        results.getInt("phone") + " " +
+                        results.getString("email"));
+            }
+
+            results.close();
 
             statement.close();
             conn.close();
