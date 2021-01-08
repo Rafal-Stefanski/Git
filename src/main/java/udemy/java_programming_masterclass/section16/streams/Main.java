18858fd2ebaaa4fdc7afe70051c5b152ccb2fa37
Rafał Stefański <rafal-stefanski@wp.pl>
2021-01-08 18:11:26 +0100

    303. Streams - Flatmap & Lambda Best Practices 2/2 with fix (M)

--- src/main/java/udemy/java_programming_masterclass/section16/streams/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section16/streams/Main.java
@@ -3,6 +3,8 @@ package udemy.java_programming_masterclass.section16.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -48,10 +50,10 @@ public class Main {
         System.out.println(concatStream.distinct().count());
 
         System.out.println("-----------------------");
-        System.out.println(concatStream
-                .distinct()
-                .peek(System.out::println)
-                .count());
+//        System.out.println(concatStream
+//                .distinct()
+//                .peek(System.out::println)
+//                .count());
 
         /** Flatmap & Lambdas */
         Employee john = new Employee("John Doe", 30);
@@ -74,6 +76,47 @@ public class Main {
                 .flatMap(department -> department.getEmployees().stream())
                 .forEach(System.out::println);
 
-        // needed fix.. getting @hexadecimal values instead of names of employees :(
+        // 07-01-2021 needed fix.. getting @hexadecimal values instead of names of employees :(
+        // 08-01-2021 FOUND IT!! - problem appeared in Employee class. Missing @override toString method, now names are printed out it original String form :D
+
+        System.out.println("-----------------------");
+//        List<String> sortedGNumbers = someBingoNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.startsWith("G"))
+//                .sorted()
+//                .collect(Collectors.toList());
+
+        List<String> sortedGNumbers = someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        for (String s : sortedGNumbers) {
+            System.out.println(s);
+        }
+
+        System.out.println("-----------------------");
+        /** collecting using grouping by method */
+        System.out.println("The youngest employee:");
+        Map<Integer, List<Employee>> groupByAge = departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+        System.out.println("-----------------------");
+        Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
+                .filter(s -> {
+                    System.out.println(s);
+                    return s.length() == 3;
+                })
+                .count();
+
     }
 }
