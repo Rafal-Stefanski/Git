bdf0462f48e1044d639f780c5874bd7cf301c3f4
Rafał Stefański <rafal-stefanski@wp.pl>
2021-01-12 16:08:01 +0100

    366. URL Connections and Input Stream Reader (M)

--- src/main/java/udemy/java_programming_masterclass/section20/high_level_api/Main.java
+++ src/main/java/udemy/java_programming_masterclass/section20/high_level_api/Main.java
@@ -9,15 +9,27 @@ public class Main {
     public static void main(String[] args) {
         try {
 //            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            URI baseUri = new URI("db://username:password@myserver.com:5000");
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedUri = baseUri.resolve(uri);
+            URI baseUri = new URI("http://username:password@myserver.com:5000");
+            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
+            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
+            URI uri3 = new URI("/stores/locations/zip=12345");
 
-            URL url = resolvedUri.toURL();
-            System.out.println("URL = " + url);
+
+            URI resolvedUri1 = baseUri.resolve(uri1);
+            URI resolvedUri2 = baseUri.resolve(uri2);
+            URI resolvedUri3 = baseUri.resolve(uri3);
+
+            URL url1 = resolvedUri1.toURL();
+            System.out.println("URL 1 = " + url1);
+            URL url2 = resolvedUri2.toURL();
+            System.out.println("URL 2 = " + url2);
+            URL url3 = resolvedUri3.toURL();
+            System.out.println("URL 3 = " + url3);
+
+            URI relativizedURI = baseUri.relativize(resolvedUri2);
+            System.out.println("Relative URI = " + relativizedURI);
 
 //            URI uri = new URI("hello");
-
 
             // checking fragments of URI above.
 //            System.out.println("Scheme = " + uri.getScheme());
---
+++ src/main/java/udemy/java_programming_masterclass/section20/url_conn_and_input_stream_reader/Main.java
@@ -0,0 +1,48 @@
+package udemy.java_programming_masterclass.section20.url_conn_and_input_stream_reader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://example.org");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+
+
+            // **************** Retrieved HTML from example.org
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
+
+
+            // ***************** checking fragments of URI above.
+//            URI uri = url.toURI();
+//
+//            System.out.println("Scheme = " + uri.getScheme());
+//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+//            System.out.println("Authority = " + uri.getAuthority());
+//            System.out.println("User info = " + uri.getAuthority());
+//            System.out.println("Host = " + uri.getHost());
+//            System.out.println("Port  = " + uri.getPort());
+//            System.out.println("Path  = " + uri.getPath());
+//            System.out.println("Query  = " + uri.getQuery());
+//            System.out.println("Fragment  = " + uri.getFragment());
+
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
